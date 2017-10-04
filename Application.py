fafe581b02f43ea9b5ed988e497b18ae309514c5
Marcel Herd <me@marcelherd.com>
2017-10-04 11:13:13 +0200

    Initial commit

---
+++ Application.py
@@ -0,0 +1,19 @@
+import os
+import discord
+import asyncio
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!contribute'):
+        await client.send_message(message.channel, 'https://github.com/marcelherd/SEP-Dumbledore')
+
+client.run(os.environ['DISCORD_TOKEN'])
